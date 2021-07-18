280b8f69da15b520d1f7679fec38840dba0b7f5c
kennyendowed <kenneyg50@gmail.com>
2021-07-18 06:57:12 +0100

    tokey

--- generate_key.php
+++ generate_key.php
@@ -1,6 +1,6 @@
 <?php
 
-$secret = bin2hex(random_bytes(20));
+$secret = bin2hex(random_bytes(20).rand(1000,9980) / 1000);
 echo "Secret:\n";
 echo $secret;
 echo "\nCopy this key to the .env file like this:\n";
