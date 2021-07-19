dc572f7b14930763154842c8517f603b68a6fb67
kennyendowed <kenneyg50@gmail.com>
2021-07-19 05:17:16 +0100

    rtr

--- controllers/helpers/validate.js
+++ controllers/helpers/validate.js
@@ -6,7 +6,13 @@ const registerValidation= (data) =>{
         name: Joi.string().min(6).required(),
         username: Joi.string().min(6).required(),
         email: Joi.string().min(6).email().required(),
-        password: Joi.string().min(8).required(),
+        phone: Joi.number().required(),
+        birthyear: Joi.number().integer().min(1970).max(2013), 
+        password: Joi.string().uppercase().lowercase().min(3).max(15).required().label('Password'),
+        password_confirmation: Joi.any().equal(Joi.ref('password'))
+            .required()
+            .label('Confirm password')
+            .options({ messages: { 'any.only': '{{#label}} does not match'} })
     });
    
        return schema.validate(data);
